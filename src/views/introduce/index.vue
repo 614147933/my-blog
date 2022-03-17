f0732935fe25af4a422bafddf994ae5b11ec189d
xiong <614147933@qq.com>
2022-03-18 00:11:50 +0800

    ### 优化 -优化首页 -自定义hook屏幕大小检查

--- src/views/introduce/index.vue
+++ src/views/introduce/index.vue
@@ -1,23 +1,61 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { defineComponent, ref, watch } from 'vue'
+import useWindowResize from "../../utils/hooks/useWindowResize";
 export default defineComponent({
     setup() {
-        
+        // 屏幕高度，宽度hook
+        const { width, height } = useWindowResize();
+        return {
+            height
+        }
     },
 })
 </script>
 <template>
-    <h1>hello 小熊</h1>
-    <div class="introduce-backgroundgreen"></div>
+    <div class="page-wrapper">
+        <div class="banner" :style="{ height: height + 'px' }">
+            <div class="banner-img">
+                <img src="../../assets/images/bar.png" />
+            </div>
+            <div class="banner-backgroundgreen" :style="{ height: height + 'px' }"></div>
+        </div>
+    </div>
+    <div class="bottom"></div>
 </template>
 <style lang="less" scoped>
-.introduce-backgroundgreen{
-    background-image: url('../../assets/images/bg-green.png');
-    position: absolute;
-    top: 0;
-    height: 100%;
-    width: 100%;
-    z-index: -1;
+.page-wrapper {
+    display: block;
+
+    .banner-img {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-40%) translateY(-50%);
+        z-index: 99;
+        img {
+            height: 500px;
+            width: 500px;
+        }
+    }
+    .banner-backgroundgreen {
+        background-image: url("../../assets/images/bg-green.png");
+        position: absolute;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        z-index: -1;
+    }
+
+    .second {
+        height: 200px;
+        width: 100%;
+        background: red;
+        position: relative;
+    }
+}
+.bottom {
+    height: 150px;
+    background: #303358;
+    position: relative;
 }
 </style>
