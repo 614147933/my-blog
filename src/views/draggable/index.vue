cc947c3e95751f90c69eb6947c6cdcf639cc7093
xiong <614147933@qq.com>
2022-04-06 23:45:08 +0800

    ### 1.draggabel优化

--- src/views/draggable/index.vue
+++ src/views/draggable/index.vue
@@ -1,89 +1,277 @@
+//参考 https://blog.csdn.net/m0_37480079/article/details/88018402?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_title~default-0.pc_relevant_default&spm=1001.2101.3001.4242.1&utm_relevant_index=3
 <template>
-    <div>
-        <div class="drop-zone">
-            <div v-for="item in listOne" :key="item.title" class="drag-el">{{ item.title }}</div>
-        </div>
-        <div class="drop-zone">
-            <div
-                v-for="item in listTwo"
-                :key="item.title"
-                class="drag-el"
-                draggable
-                @dragstart="startDrag($event, item)"
-            >{{ item.title }}</div>
-        </div>
+  <div class="container">
+    <div class="layout">
+      <button
+        class="layout-btn"
+        @click="layoutType = val.value"
+        v-for="val in layoutOptions"
+        :key="val.value"
+      >{{ val.label }}</button>
     </div>
+
+    <div
+      class="group"
+      :class="{
+        'left-top-container': gindex === 0,
+        'right-top-container': gindex === 1,
+        'bottom-container': gindex === 2,
+        'top-container': gindex < 2
+      }"
+      v-for="(group, gindex) in data"
+      :key="gindex"
+    >
+      <div
+        class="cls-default"
+        v-for="(item, cindex) in group.children"
+        :key="cindex"
+        :data-id="gindex + '-' + cindex"
+        draggable="true"
+        @dragstart="onDragstart($event)"
+        @dragend="onDragend($event)"
+        @dragover="onDragover($event)"
+        @drop="onDrop($event)"
+        :style="{ 'background-color': item.color }"
+        :class="{
+          'cls1-0': cindex === 0 && layoutType == 1,
+          'cls2-0': (cindex === 0 || cindex === 1) && layoutType == 2,
+          'cls3-0': cindex === 0 && layoutType == 3,
+          'cls3-1': (cindex === 1 || cindex === 2) && layoutType == 3,
+          'cls4-0': cindex < 4 && layoutType == 4,
+          'cls6-0': cindex === 0 && layoutType == 6
+        }"
+      >
+        <div class="content">{{ item.color ? item.color : '我是空对象' }}</div>
+      </div>
+    </div>
+    <div class="tips">
+      上面两个区域内是展示区的内容能互相拖拽
+      <br />下面的是资源区，只能复制出去覆盖目标区域，本身不会被替换掉
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, computed } from 'vue'
+import { defineComponent, reactive, computed, ref } from 'vue'
 
 export default defineComponent({
-    setup() {
-        let items = reactive([
-            {
-                id: 0,
-                title: 'Item A',
-                list: 1
-            },
-            {
-                id: 1,
-                title: 'Item B',
-                list: 1
-            },
-            {
-                id: 2,
-                title: 'Item C',
-                list: 2
-            }
-        ])
-        const listOne = computed(() => {
-            return items.filter(item => item.list === 1)
-        })
+  setup() {
+    let stargindex = ref('')
+    let endIndex = ref('')
+    let layoutType = ref(9)
+    let layoutOptions = reactive([
+      { label: "单分屏", value: 1 },
+      { label: "二分屏", value: 2 },
+      { label: "三分屏", value: 3 },
+      { label: "四分屏", value: 4 },
+      { label: "六分屏", value: 6 },
+      { label: "九分屏", value: 9 }
+    ])
+    let data = reactive([
+      {
+        group: "left-show",
+        title: "视频播放区一",
+        children: [
+          {
+            id: 6,
+            color: "orange"
+          },
+          {
+            id: 2,
+            color: "yellow"
+          },
+          {},
+          {},
+          {},
+          {},
+          {
+            id: 3,
+            color: "cyan"
+          },
+          {},
+          {
+            id: 5,
+            color: "brown"
+          }
+        ]
+      },
+      {
+        group: "right-show",
+        title: "视频播放区二",
+        children: [
+          {},
+          {
+            id: 7,
+            color: "pink"
+          },
+          {},
+          {},
+          { id: 4, color: "purple" },
+          {},
+          {},
+          {},
+          {
+            id: 10,
+            color: "gray"
+          }
+        ]
+      },
+      {
+        group: "source",
+        title: "视频资源区",
+        children: [
+          {
+            id: 11,
+            color: "white"
+          },
+          {
+            id: 12,
+            color: "black"
+          },
+          {
+            id: 13,
+            color: "red"
+          },
+          {
+            id: 14,
+            color: "green"
+          },
+          {
+            id: 15,
+            color: "blue"
+          }
+        ]
+      }
+    ])
 
-        const listTwo = computed(() => {
-            return items.filter(item => item.list === 2)
-        })
-
-        const startDrag = (evt: any, item: any) => {
-            evt.dataTransfer.dropEffect = 'move'
-            evt.dataTransfer.effectAllowed = 'move'
-            evt.dataTransfer.setData('itemID', item.id)
-        }
-
-        const onDrop = (evt: any, list: any) => {
-            const itemID = evt.dataTransfer.getData('itemID')
-            items.find(item => item.id == itemID)
-        }
-
-
-        return {
-            listOne,
-            listTwo,
-            onDrop
-        }
-    },
-    methods: {
-        startDrag: (evt: any, item: any) => {
-            console.log('触发了事件');
-            evt.dataTransfer.dropEffect = 'move'
-            evt.dataTransfer.effectAllowed = 'move'
-            evt.dataTransfer.setData('itemID', item.id)
-        },
-
-
+    const onDragstart =(event:any) => {
+      stargindex = event.target.getAttribute("data-id");
     }
+    const onDragend = () => {
+      let startGroupIndex = stargindex.split("-")[0];
+      let startChildIndex = stargindex.split("-")[1];
+      let endGroupIndex = endIndex.split("-")[0];
+      let endChildIndex = endIndex.split("-")[1];
+      // 对数据做简单的深拷贝 目前不需要
+      // let endObj = JSON.parse(
+      //   JSON.stringify(this.data[endGroupIndex].children[endChildIndex])
+      // );
+      // let startObj = JSON.parse(
+      //   JSON.stringify(this.data[startGroupIndex].children[startChildIndex])
+      // );
+      let endObj = data[endGroupIndex].children[endChildIndex];
+      let startObj = data[startGroupIndex].children[startChildIndex];
+      if (data[endGroupIndex].group === "source") {
+        //往资源区拖拽时  不做任何替换操作
+        return;
+      }
+      data[endGroupIndex].children.splice(endChildIndex, 1, startObj);
+      if (data[startGroupIndex].group !== "source") {
+        //拖拽起始区域不是 source时  把起始区域替换成拖拽后区域的数据
+        data[startGroupIndex].children.splice(startChildIndex, 1, endObj);
+      }
+    }
+    const onDrop = (event:any) => {
+      if (event.target.className.indexOf("cls-default") > -1) {
+        endIndex = event.target.getAttribute("data-id");
+      } else {
+        endIndex = event.target.parentElement.getAttribute("data-id");
+      }
+    }
+    const onDragover = (event:any) =>{
+      event.preventDefault();
+    }
+    return {
+      stargindex,
+      endIndex,
+      layoutType,
+      layoutOptions,
+      data,
+      onDragstart,
+      onDragend,
+      onDrop,
+      onDragover
+    }
+  },
+  methods: {
+  }
 })
 </script>
 <style lang="less" scoped>
-.drop-zone {
-    background-color: #eee;
-    margin-bottom: 10px;
-    padding: 10px;
+.container {
+  background-color: #eee;
+  height: 800px;
 }
-
-.drag-el {
-    background-color: #fff;
-    margin-bottom: 10px;
-    padding: 5px;
+.layout .layout-btn {
+  background-color: #409eff;
+  color: #fff;
+  padding: 10px 15px;
+  margin: 10px 15px;
+}
+.tips {
+  font-size: 24px;
+  text-align: center;
+}
+.group {
+  float: left;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+.group-title {
+  height: 40px;
+  line-height: 40px;
+}
+.cls-default {
+  float: left;
+  margin: 0;
+  box-sizing: border-box;
+  overflow: hidden;
+  border: 1px solid #999;
+}
+.cls-default .content {
+  text-align: center;
+  padding-top: 20px;
+  font-size: 20px;
+}
+.top-container {
+  height: 400px;
+  width: 40%;
+  margin: 15px 5%;
+}
+.top-container .cls-default {
+  width: 33.33%;
+  height: 33.33%;
+}
+.top-container .cls1-0 {
+  width: 100%;
+  height: 100%;
+}
+.top-container .cls2-0 {
+  width: 50%;
+  height: 100%;
+}
+.top-container .cls3-0 {
+  width: 50%;
+  height: 100%;
+}
+.top-container .cls3-1 {
+  width: 50%;
+  height: 50%;
+}
+.top-container .cls4-0 {
+  width: 50%;
+  height: 50%;
+}
+.top-container .cls6-0 {
+  width: 66.66%;
+  height: 66.65%;
+}
+.bottom-container {
+  width: 90%;
+  height: 200px;
+  margin: 15px 5%;
+}
+.bottom-container .cls-default {
+  width: 15%;
+  height: 150px;
 }
 </style>
